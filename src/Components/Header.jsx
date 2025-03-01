e4293f92c44356e56c44a3dbff23d68c763bb818
Kerem Taşdemir <tast.kerem@gmail.com>
2025-03-01 14:35:31 +0300

    asas

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -15,32 +15,52 @@ function Header() {
 
                 <h1 onClick={() => navigate('/')} className='font-bold cursor-pointer text-4xl pr-6'>LOGO</h1>
                 <div className='h-10 w-0.5 bg-white'></div>
-                <div className='flex justify-center pr-20 cursor-pointer font-serif text-xl pl-6 mt-1.5 space-x-6'>
-                    <h1 onClick={() => navigate('/')} className=" hover:text-orange-600 duration-300">Anasayfa</h1>
-                    <h1 onClick={() => navigate('/about')} className=" hover:text-orange-600 duration-300">Biz Kimiz?</h1>
-                    <h1 className=" hover:text-orange-600 duration-300">Menü</h1>
-                    <h1 className=" hover:text-orange-600 duration-300 pr-1">Blog</h1>
+                <div className='flex justify-center pr-20 cursor-pointer font-serif  text-lg pl-6 mt-1.5 space-x-6'>
+                    <h1 onClick={() => navigate('/')} className="relative hover:text-orange-600 duration-300 cursor-pointer group text-xl">
+                        Anasayfa
+                        <span className="absolute left-1/2 bottom-0 w-0 h-0.5 bg-orange-600 transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+                    </h1>
+                    <h1 onClick={() => navigate('/about')} className="relative hover:text-orange-600 duration-300 cursor-pointer group text-xl">
+                        Biz Kimiz?
+                        <span className="absolute left-1/2 bottom-0 w-0 h-0.5 bg-orange-600 transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+                    </h1>
+                    <h1 className="relative hover:text-orange-600 duration-300 cursor-pointer group text-xl">
+                        Menü
+                        <span className="absolute left-1/2 bottom-0 w-0 h-0.5 bg-orange-600 transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+                    </h1>
+                    <h1 className="relative hover:text-orange-600 duration-300 cursor-pointer group pr-1 text-xl">
+                        Blog
+                        <span className="absolute left-1/2 bottom-0 w-0 h-0.5 bg-orange-600 transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+                    </h1>
+
                 </div>
 
                 <div className='relative' onMouseEnter={() => isSetOpen(true)} onMouseLeave={() => isSetOpen(false)}>
 
-                    <h1 className='hover:text-orange-600 duration-300 cursor-pointer font-serif text-xl pl-3 mt-1.5 -ml-20'>Bize Ulaşın<ExpandMoreIcon fontSize='small' className='mb-0.5' /></h1>
+                    <h1 className="relative hover:text-orange-600 duration-300 cursor-pointer font-serif text-xl pl-6 mt-1.5 -ml-20 group inline-block ">
+                        <span className="relative flex items-center">
+                            Bize Ulaşın
+                            <ExpandMoreIcon fontSize="small" className="mb-0.5 ml-1" />
+                        </span>
+                        <span className="absolute left-1/2 bottom-0 w-0 h-[2px] bg-orange-600 transition-all duration-300 ease-out group-hover:w-full group-hover:left-0"></span>
+                    </h1>
+
+
                     {isOpen && (
                         <div className="absolute left-0 mt-2 w-40 bg-slate-800 text-white border-gray-300 rounded-sm shadow-lg">
                             <ul className="py-2">
-                                <li onClick={handleOptionClick} className="px-4 py-2 hover:text-orange-700 duration-300 cursor-pointer">
-                                    İletişim
-                                </li>
-                                <li onClick={handleOptionClick} className="px-4 py-2 hover:text-orange-700 duration-300 cursor-pointer">
-                                    AAAAA
-                                </li>
-                                <li onClick={handleOptionClick} className="px-4 py-2 hover:text-orange-700 duration-300 cursor-pointer">
-                                    BBBBBB
-                                </li>
-                                <li onClick={handleOptionClick} className="px-4 py-2 hover:text-orange-700 duration-300 cursor-pointer">
-                                    CCCCCC
-                                </li>
+                                {["İletişim", "AAAAA", "BBBBBB", "CCCCCC"].map((item, index) => (
+                                    <li
+                                        key={index}
+                                        onClick={handleOptionClick}
+                                        className="px-4 py-2 hover:text-orange-600 duration-300 cursor-pointer relative group"
+                                    >
+                                        {item}
+                                        <span className="absolute left-1/2 bottom-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+                                    </li>
+                                ))}
                             </ul>
+
                         </div>
                     )}
                 </div>
